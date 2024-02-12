c2b2d25bc1e276024efae9c2a74c1bbd8efd1430
Chan <chan@iPod-Classic-128GB.local>
2024-02-12 13:58:00 +0000

    added testing component

---
+++ src/CustomButton.d.ts
@@ -0,0 +1,7 @@
+import { TouchableOpacityProps } from "react-native";
+
+export interface CustomButtonProps extends TouchableOpacityProps {
+  title: string;
+}
+
+export const CustomButton: React.FC<CustomButtonProps>;
